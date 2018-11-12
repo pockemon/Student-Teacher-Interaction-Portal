d2d23582c47e2ababe403c2ae3182f8863e7678c
Hardik Rana <hardikrana276@gmail.com>
2018-11-12 04:25:28 +0200

    Delete footer.php

--- includes/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="navbar-fixed-bottom">
-<div class="container">
-<center>
-<p>Copyright &copy; Lifestyle Store. All Rights Reserved | Contact Us: +91 90000
-00000</p>
-</center>
-</div>
-</footer>
